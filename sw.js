dc6aa66b15762289770c396b6586f8d70cd8f9b0
Aapv16 <afrianusvicky38@gmail.com>
2025-05-25 12:50:24 +0700

    Updates

--- sw.js
+++ sw.js
@@ -1,31 +1,31 @@
-// const cacheName = "NsiAsisstent";
-// const preCache = ["/", "/style.css", "/script.js"];
+const cacheName = "NsiAsisstent";
+const preCache = ["/", "/style.css", "/script.js"];
 
-// self.addEventListener("instal", () => {
-//   console.log("service worker installed");
+self.addEventListener("instal", () => {
+  console.log("service worker installed");
 
-//   e.waitUntil(
-//     (async () => {
-//       const cache = await caches.open(cacheName);
-//       cache.addAll(preCache);
-//     })()
-//   );
-// });
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      cache.addAll(preCache);
+    })()
+  );
+});
 
-// self.addEventListener("fetch", (e) => {
-//   e.respondWith(
-//     (async () => {
-//       const cache = await caches.open(cacheName);
-//       const resCache = await cache.match(e.request);
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      const resCache = await cache.match(e.request);
 
-//       if (resCache) return resCache;
-//       try {
-//         const res = await fetch(e.request);
-//         cache.put(e.request, res.clone());
-//         return res;
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     })()
-//   );
-// });
+      if (resCache) return resCache;
+      try {
+        const res = await fetch(e.request);
+        cache.put(e.request, res.clone());
+        return res;
+      } catch (error) {
+        console.log(error);
+      }
+    })()
+  );
+});
