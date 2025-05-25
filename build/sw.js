baacb29585779d08e551af6a9a4019638bcc03be
Aapv16 <afrianusvicky38@gmail.com>
2025-05-25 23:05:53 +0700

    sw

--- build/sw.js
+++ build/sw.js
@@ -1,31 +1,43 @@
-// const cacheName = "NsiAsisstent";
-// const preCache = ["/", "/style.css", "/script.js"];
+const cacheName = "NsiAssistant-v2"; // Ganti versi jika update
+const preCache = ["/", "/style.css", "/script.js"];
 
-// self.addEventListener("instal", () => {
-//   console.log("service worker installed");
+self.addEventListener("install", (e) => {
+  console.log("Service Worker installed");
 
-//   e.waitUntil(
-//     (async () => {
-//       const cache = await caches.open(cacheName);
-//       cache.addAll(preCache);
-//     })()
-//   );
-// });
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(preCache);
+    })
+  );
+});
 
-// self.addEventListener("fetch", (e) => {
-//   e.respondWith(
-//     (async () => {
-//       const cache = await caches.open(cacheName);
-//       const resCache = await cache.match(e.request);
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== cacheName)
+            .map((key) => caches.delete(key))
+        )
+      )
+  );
+});
 
-//       if (resCache) return resCache;
-//       try {
-//         const res = await fetch(e.request);
-//         cache.put(e.request, res.clone());
-//         return res;
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     })()
-//   );
-// });
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(e.request).then((networkResponse) => {
+        return caches.open(cacheName).then((cache) => {
+          cache.put(e.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
